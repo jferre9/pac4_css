66b2e96edda4d207151a8fe1467a399d7e1da465
unknown <a>
2016-12-12 16:10:12 +0100

    a

--- index.php
+++ index.php
@@ -23,58 +23,10 @@
     <script src="js/parallax.js"></script>
 </head>
 
+    
+    <!-- TODO: posar links a tot -->
 <body>
-    <header>
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header pull-left"> <a class="navbar-brand" href="#">Brand</a> </div>
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header pull-right">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                    <div class="navbar-header pull-right">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="pull-right"><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-                            <li class="pull-right"><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-                            <li class="pull-right"><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">HOME <span class="sr-only">(current)</span></a></li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MEGAMENU <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">SHOP</a></li>
-                        <li><a href="#">BLOG</a></li>
-                        <li><a href="#">PORTFOLIO</a></li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PAGES <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container-fluid -->
-        </nav>
-    </header>
+    <?php include "header.html" ?>
     <!-- ################################################################## -->
     <main>
         <div class="container productes">
@@ -166,99 +118,14 @@
         <?php include "slider4.html" ?>
         <!-- ###################################################### -->
         
-        <div class="newsletter parallax-window" androidFix="false" data-parallax="scroll" data-image-src="imatges/parallax1.jpg">
-            <div class="container">
-                <div class="row">
-                    <div class="text-blanc text-center">
-                        <h2>SUBSCRIBE TO OUR NEWSLETTER</h2>
-                        <p>Permanence of the stars Drake Equation emerged into consciousness.</p>
-                    </div>
-                    <form>
-                        <div class="col-xs-2"></div>
-                        <!-- Posant offsets no anava be -->
-                        <div class="form-group col-sm-7 col-xs-8">
-                            <input type="text" placeholder="keyword search..." class="form-control formulari-transparent"> </div>
-                        <div class="form-group col-sm-3 col-xs-12 centrar-petit">
-                            <button type="submit" class="btn boto-taronja">SUBSCRIBE</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <!-- ############# -->
-        <div class="container abansFooter">
-            <div class="row">
-                <div class="col-md-4"> <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                    <h3>Free Shipping Worldwide</h3>
-                    <p>On order over 150$ - 7 week</p>
-                </div>
-                <div class="col-md-4"> <i class="fa fa-money" aria-hidden="true"></i>
-                    <h3>Money Back Guarantee</h3>
-                    <p>Send within 30 days</p>
-                </div>
-                <div class="col-md-4"> <i class="fa fa-clock-o" aria-hidden="true"></i>
-                    <h3>24/7 Customer Services</h3>
-                    <p>Call us 24/7 at 2568-88</p>
-                </div>
-            </div>
-        </div>
+        <?php include "abans_footer.html" ?>
+        
         
     </main>
     <!-- ################################################################## -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-3 col-md-push-9">
-                    <h3 class="logotip">FABRIC</h3>
-                    <p>His duobus, sicariorum. Et orci aetate erat, sed nescio quo modo se gerendo levir meus aufert a me.</p>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <h4>ABOUT</h4>
-                    <ul class="fa-ul">
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Customer Service</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Careers</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Help</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Shipping Information</li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <h4>OUR STORY</h4>
-                    <ul class="fa-ul">
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Where To Buy</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Wholesale</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Offers</li>
-                        <li><i class="fa-li fa fa-angle-double-right" aria-hidden="true"></i>Discount Off</li>
-                    </ul>
-                </div>
-                <div class="col-xs-10 col-sm-6 col-md-3 col-md-pull-9">
-                    <div class="itemFooter">
-                        <button type="button" class="btn btn-default"><i class="fa fa-home" aria-hidden="true"></i></button>
-                        <p> 27, North circular road,
-                            <br> New York city. </p>
-                    </div>
-                    <div class="itemFooter">
-                        <button type="button" class="btn boto-taronja"><i class="fa fa-envelope-o" aria-hidden="true"></i></button>
-                        <p> info@yourname.com
-                            <br> www.yourweb.com </p>
-                    </div>
-                    <div class="itemFooter">
-                        <button type="button" class="btn btn-default"><i class="fa fa-phone" aria-hidden="true"></i></button>
-                        <p> +088-653-221-55
-                            <br> +088-332-777-11 </p>
-                    </div>
-                </div>
-                <div class="col-xs-2 col-sm-6 col-md-3 col-md-push-9 social"> <i class="fa fa-facebook" aria-hidden="true"></i> <i class="fa fa-twitter" aria-hidden="true"></i> <i class="fa fa-google-plus" aria-hidden="true"></i> <i class="fa fa-connectdevelop" aria-hidden="true"></i> <i class="fa fa-behance" aria-hidden="true"></i> <i class="fa fa-youtube" aria-hidden="true"></i> </div>
-            </div>
-        </div>
-        <div id="finalDeTot">
-            <div class="container">
-                <div class="col-sm-6">
-                    <p>Copyright &copy;2016 All right reserved</p>
-                </div>
-                <div class="col-sm-6 hidden-xs"> fotos </div>
-            </div>
-        </div>
-    </footer>
+    
+    <?php include "footer.html" ?>
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
